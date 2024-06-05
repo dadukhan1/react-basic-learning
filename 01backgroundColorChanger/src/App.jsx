957deab598daef68fd643b2dfbba0b240b95642b
Dadu Khan <photoshopt65@gmail.com>
2024-06-05 06:10:17 -0700

    bgfinish

--- 01backgroundColorChanger/src/App.jsx
+++ 01backgroundColorChanger/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 function App() {
-  const [color, setcolor] = useState("olive")
+  const [color, setcolor] = useState("")
   return (
     <div className='w-full h-screen duration-200 flex justify-center'
     style={{backgroundColor: color}}
@@ -9,14 +9,19 @@ function App() {
       <div className='fixed flex flex-wrap justify-center bottom-12 insert-x-0 px-2 '>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
           <button className='outline-none px-4 py-1 rounded-full text-white' 
+          onClick={() => setcolor("red")}
           style={{backgroundColor:"red"}}>Red</button>
           <button className='outline-none px-4 py-1 rounded-full text-white' 
+          onClick={() => setcolor("green")}
           style={{backgroundColor:"green"}}>Green</button>
           <button className='outline-none px-4 py-1 rounded-full text-white' 
+          onClick={() => setcolor("gold")}
           style={{backgroundColor:"gold"}}>Gold</button>
           <button className='outline-none px-4 py-1 rounded-full text-white' 
+          onClick={() => setcolor("crimson")}
           style={{backgroundColor:"crimson"}}>Crimson</button>
           <button className='outline-none px-4 py-1 rounded-full text-white' 
+          onClick={() => setcolor("midnightblue")}
           style={{backgroundColor:"midnightblue"}}>Blue</button>
           
         </div>
---
+++ filmApp/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <button style={{backgroundColor:"green", color:""}}>Hehe</button>
+    </div>
+  )
+}
+
+export default App
